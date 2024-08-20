c5e1280e1fb226cbee1fd6d83b9bfe62dadacd8e
Jonathan Samuels <jonathansamuels@Jonathans-MacBook-Pro-2.local>
2024-08-19 20:16:23 -0700

    Challenge 1: Successfully imported required functions from file

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {add, multiply, subtract, divide} from "./calculator"; 
 
 //Import the add, multiply, subtract and divide functions
 //from the calculator.js file.
@@ -9,12 +10,14 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <ul>
-      <li>{add(1, 2)}</li>
-      <li>{multiply(2, 3)}</li>
-      <li>{subtract(7, 2)}</li>
-      <li>{divide(5, 2)}</li>
-    </ul>
+    <div>
+      <ul>
+        <li>{add(1, 2)}</li>
+        <li>{multiply(2, 3)}</li>
+        <li>{subtract(7, 2)}</li>
+        <li>{divide(5, 2)}</li>
+      </ul>
+    </div>
   </React.StrictMode>
 );
 
